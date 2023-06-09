dccb051e3b937293c7d1a4d0f4fe2bf2affcb054
Noor Ul Amin <noorawan444@gmail.com>
2023-06-09 23:06:42 +0500

    add post method to add write data into the db

--- server.ts
+++ server.ts
@@ -6,14 +6,37 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    const query = await client.query("SELECT * FROM recipes");
-    res.status(200).send({ success: true, data: query.rows });
-  } catch (error) {
-    res.status(500).send({ success: false, message: error });
-  }
-});
+app
+  .route("/api/recipes")
+  .get(async (req: Request, res: Response) => {
+    try {
+      const query = await client.query("SELECT * FROM recipes");
+      res.status(200).send({ success: true, data: query.rows });
+    } catch (error) {
+      res.status(500).send({ success: false, message: error });
+    }
+  })
+  .post(async (req: Request, res: Response) => {
+    const { title, description, ingredients, directions } = req.body;
+    if (!title || !description || !ingredients || !directions) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide all required fields",
+      });
+    }
+    try {
+      await client.query(
+        `
+      INSERT INTO recipes (title, description, ingredients, directions)
+      VALUES ($1, $2, $3, $4)
+    `,
+        [title, description, ingredients, directions]
+      );
+      res.status(201).send({ success: true, message: "Recipe created" });
+    } catch (error) {
+      res.status(500).send({ success: false, message: error });
+    }
+  });
 
 app.use("*", (req: Request, res: Response) => {
   res.status(404).send(`${req.originalUrl} not found`);
