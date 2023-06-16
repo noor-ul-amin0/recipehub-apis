8843a2664475497f290bc024af03cc45f9845765
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 14:44:03 +0500

    create sql script file to add tables

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -9,3 +9,31 @@ CREATE TABLE IF NOT EXISTS recipes (
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- Add user_id column to recipes table
+-- Check if the column 'user_id' already exists in the 'recipes' table
+DO $$ BEGIN
+  IF NOT EXISTS (
+    SELECT FROM information_schema.columns
+    WHERE table_name = 'recipes' AND column_name = 'user_id'
+  ) THEN
+    -- Add the 'user_id' column to the 'recipes' table
+    ALTER TABLE recipes
+    ADD COLUMN user_id INT;
+
+    -- Add foreign key constraint
+    ALTER TABLE recipes
+    ADD CONSTRAINT fk_user_id
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE;
+  END IF;
+END $$;
+
