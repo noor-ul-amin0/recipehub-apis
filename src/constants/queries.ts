ace5f48dd2a762e27cdb19f2e18ab4e8dae73e8d
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 14:51:23 +0500

    add db queries constant

---
+++ src/constants/queries.ts
@@ -0,0 +1,20 @@
+export const findAllRecipesQuery = "SELECT * FROM recipes WHERE user_id = $1";
+
+export const findRecipesBySearchQuery = `
+  SELECT *
+  FROM recipes
+  WHERE user_id = $2 AND to_tsvector(title || ' ' || description || ' ' || array_to_string(ingredients,'') || ' ' || array_to_string(directions,'')) @@ plainto_tsquery($1)
+`;
+
+export const createRecipeQuery = `
+  INSERT INTO recipes (title, description, ingredients, directions, user_id)
+  VALUES ($1, $2, $3, $4, $5)
+`;
+
+export const createUserQuery = `
+  INSERT INTO users (full_name, email, password)
+  VALUES ($1, $2, $3)
+`;
+
+export const findUserByEmailQuery = `
+  SELECT * FROM users WHERE email = $1 LIMIT 1`;
