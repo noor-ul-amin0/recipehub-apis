463ba4477810ba07e768be36d64d9c8c9b6430c9
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 14:51:44 +0500

    add recipes controllers

--- src/controllers/recipes.ts
+++ src/controllers/recipes.ts
@@ -8,26 +8,43 @@ export const getRecipes = async (
 ): Promise<void> => {
   try {
     const { search } = req.query;
-    let recipes: Recipe[];
-    if (search) {
-      recipes = await recipeRepository.findBySearchQuery(search as string);
-    } else {
-      recipes = await recipeRepository.findAll();
+    let recipes: Recipe[] = [];
+    if (req.user) {
+      if (search) {
+        recipes = await recipeRepository.findBySearchQuery(
+          search as string,
+          req?.user.id
+        );
+      } else {
+        recipes = await recipeRepository.findAll(req.user.id);
+      }
     }
-    res
-      .status(200)
-      .send({ success: true, count: recipes.length, data: recipes });
+    res.status(200).send({ success: true, data: recipes });
   } catch (error) {
     res.status(500).send({ success: false, message: error });
   }
 };
 
 export const createRecipe = async (
-  req: Request,
+  req: Request<never, never, never, never>,
   res: Response
 ): Promise<void> => {
-  const { title, description, ingredients, directions } = req.body;
-  if (!title || !description || !ingredients || !directions) {
+  const { title, description, ingredients, directions }: Recipe = req.body;
+  if (!req.user) {
+    res.status(401).send({
+      success: false,
+      message: "Something went wrong",
+    });
+    return;
+  }
+  if (
+    !title ||
+    !description ||
+    !ingredients ||
+    !directions ||
+    !ingredients?.length ||
+    !directions?.length
+  ) {
     res.status(400).send({
       success: false,
       message: "Please provide all required fields",
@@ -35,7 +52,7 @@ export const createRecipe = async (
     return;
   }
   try {
-    await recipeRepository.create(req.body);
+    await recipeRepository.create(req.body, req.user.id);
     res.status(201).send({ success: true, message: "Recipe created" });
   } catch (error) {
     res.status(500).send({ success: false, message: error });
