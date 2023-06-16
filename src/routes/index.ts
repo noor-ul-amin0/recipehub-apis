1b3fb0d7b76adf811c0e72f1b60e5e033dbb565c
Noor Ul Amin <noorawan444@gmail.com>
2023-06-16 14:54:54 +0500

    initialize users routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import recipeRoutes from "./recipes";
+import userRoutes from "./users";
+
 const router = express.Router();
 
 router.use("/api/recipes", recipeRoutes);
+router.use("/api/users", userRoutes);
 
 export default router;
