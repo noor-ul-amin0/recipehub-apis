cf6bc250d7d6220010adb1e5d0928290de1308b1
Noor Ul Amin <noorawan444@gmail.com>
2023-07-24 15:44:14 +0500

    Add verify-email by token endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,4 +9,5 @@ router.post("/signup", userController.signup);
 // User signin route
 router.post("/signin", userController.signin);
 
+router.put("/verify-email/:token", userController.verifyEmail);
 export default router;
