8c7486876cf00a21548014270101fe306efd956c
Noor Ul Amin <noorawan444@gmail.com>
2023-08-10 18:05:42 +0500

    Move server.ts into src dir

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import expressWinston from "express-winston";
 import winston from "winston";
-import routes from "./src/routes";
-import { connectDB } from "./src/config/db";
+import routes from "./routes";
+import { connectDB } from "./config/db";
 
 const app: Express = express();
 app.use(cors());
@@ -15,7 +15,12 @@ connectDB();
 // express-winston logger
 app.use(
   expressWinston.logger({
-    transports: [new winston.transports.Console()],
+    level: "info",
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: "error.log", level: "error" }),
+      new winston.transports.File({ filename: "info.log", level: "info" }),
+    ],
     format: winston.format.combine(
       winston.format.colorize(),
       winston.format.json()
@@ -29,6 +34,8 @@ app.use("/", routes);
 app.use("*", (req: Request, res: Response) => {
   res.status(404).send(`${req.originalUrl} not found`);
 });
+
+// Error handling middleware
 
 const port = 8080 || process.env.PORT;
 app.listen(port, () => {
